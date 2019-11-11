165d0e1c75b244332d0a68b76b119cf3ab12d781
Gabriel <arcilli@nemesis.arrnaux.com>
2019-11-11 10:17:55 +0200

    Added initial commit for fe. No integration with user-service.

---
+++ front-end/src/main/java/com/arrnaux/frontend/Greeting.java
@@ -0,0 +1,15 @@
+package com.arrnaux.frontend;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Greeting {
+
+    private long id;
+    private String content;
+}
