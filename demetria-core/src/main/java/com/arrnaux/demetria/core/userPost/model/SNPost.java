151280a8dc3c5b92802a404f27ddbabc851b6238
arcilli <raileanu.gabriel97@gmail.com>
2019-11-28 02:18:20 +0200

    Added date field for post.

--- demetria-core/src/main/java/com/arrnaux/demetria/core/userPost/model/SNPost.java
+++ demetria-core/src/main/java/com/arrnaux/demetria/core/userPost/model/SNPost.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
 import java.util.List;
 
 @Log4j
@@ -28,4 +29,6 @@ public class SNPost {
     protected PostVisibility visibility;
 
     protected List<Vote> votes;
+
+    protected Date creationDate;
 }
