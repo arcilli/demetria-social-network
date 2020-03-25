abb8e391803ba63cece90f355a8d2d31d13a969f
arcilli <raileanu.gabriel97@gmail.com>
2020-03-25 18:09:00 +0200

    Implemented ReplicaSets on mongo. Still need to use volumes for machines.

--- deployment/stop.sh
+++ deployment/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker service rm deme_eureka-server deme_front-end deme_friendship-relation deme_user-service deme_mongo deme_orient-db deme_post-service
-
+#!/bin/sh
+docker service rm deme_eureka-server deme_front-end deme_friendship-relation deme_user-service deme_orient-db deme_post-service deme_rs1 deme_rs2 deme_rs3
