78797291c30a2e9823ff56f4cf1767b13b8931bb
arcilli <raileanu.gabriel97@gmail.com>
2020-04-12 19:49:17 +0300

    Added script that populate initial mongo db.

--- deployment/mongo-replicas/init.sh
+++ deployment/mongo-replicas/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Make sure 3 replicas available.
+# Make sure 3 replicas are available.
 for rs in rs1 rs2 rs3; do
   mongo --host $rs --eval 'db'
   #  Wait until all the replicas are ready.
@@ -14,6 +14,13 @@ if [ $? -ne 0 ]; then
   echo "ReplicaSet is not yet configured."
   mongo --host rs1 --eval 'rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host : "rs1" }, { _id: 1, host : "rs2" }, { _id: 2, host : "rs3" } ] })'
 fi
-# TODO: populate with initial data inside primary.
-#  primaryReplica=$(mongo --host rs1 --eval 'rs.isMaster().primary')
-#  echo "Primary replica: $primaryReplica"
+
+# Populate with initial data inside primary.
+echo "The replica set is configured. Populate with initial data."
+status=$(mongo --quiet --host rs1 --eval 'rs.status().members.length')
+echo "Number of members: $status."
+if [ "$status" -eq 3 ]; then
+  primaryReplica=$(mongo --quiet --host rs1 --eval 'rs.isMaster().primary')
+  echo "Primary replica: $primaryReplica"
+  mongo --host "$primaryReplica" < /usr/local/mongo-initial-load.js
+fi
--- deployment/push-deme-images.sh
+++ deployment/push-deme-images.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Try not to use this since it's using custom version of images.
+# Generalize only custom versions (or the latest one).
 docker push arcilli/demetria-user-service:2.0-SNAPSHOT
 docker push arcilli/demetria-friendship-relation-service:2.0-SNAPSHOT
 docker push arcilli/demetria-front-end:2.0-SNAPSHOT
