a51e64eefda097503c2df9e7c7ce8a2d9909f6b8
arcilli <raileanu.gabriel97@gmail.com>
2020-05-14 10:15:20 +0300

    Issue #31: Create docker image for OrientDB

--- deployment/orient-db/initial-setup/orient-setup.sh
+++ deployment/orient-db/initial-setup/orient-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# TODO: replace it with a for
 while ! $(nc -z orient-db 2424); do
   sleep 3
 done
+# If the database is already present, it's assumed that it contains already dump data.
 /orientdb/bin/console.sh "CREATE DATABASE remote:orient-db/test root rootpwd; IMPORT DATABASE initial-db.gz -preserveClusterIDs=true"
--- deployment/stack/stop.sh
+++ deployment/stack/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#!/bin/sh
-docker service rm deme_front-end deme_friendship-relation deme_user-service deme_orient-db deme_post-service deme_rs1 deme_rs2 deme_rs3 deme_search-service deme_rs-setup
+docker service rm deme_front-end deme_friendship-relation deme_user-service deme_orient-db deme_post-service deme_rs1 deme_rs2 deme_rs3 deme_search-service
+docker service rm deme_rs-setup deme_orient-db-initial-setup
