7ec97c904457fac3faabe2a77d81bb5ffabf334d
arcilli <raileanu.gabriel97@gmail.com>
2020-07-21 20:49:50 +0300

    Added work on swarm statistics.

--- deployment/statistics/containersMemoryUsage.sh
+++ deployment/statistics/containersMemoryUsage.sh
@@ -2,8 +2,16 @@
 now="$(date +'%d-%m-%Y %H:%M:%S')"
 jsonObject="{$now: "
 ## for each microservice that is available, write microservice name : memory info
-containersStatus=$("docker stats --no-stream --format '{{.Container}}: {{.MemUsage}}'")
-echo "$containerStatus"
-jsonObject="$jsonObject }"
-echo "$jsonObject"
-
+now="$(date +'%d-%m-%Y %H:%M:%S')"
+jsonObject="{$now: "
+## for each microservice that is available, write microservice name : memory info
+containersStatus="$(docker stats --no-stream --format 'table {{.Name}}: {{.MemUsage}}')"
+##echo "1: $containersStatus"
+for container in $containersStatus
+do
+  echo $container
+  #echo dgasda
+  #$jsonObject=$jsonObject$container
+done
+#jsonObject="$jsonObject }"
+#echo "$jsonObject"
